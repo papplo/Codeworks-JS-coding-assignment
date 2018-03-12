597e538146c7d185ebf30861c56a90cbbc2bdcc0
Pablo Anttila <pablo.anttila@tobii.com>
2018-03-12 13:20:34 +0100

    now click acts on array

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // Build a simple Blackjack game using HTML, CSS, JavaScript and jQuery. It’s a single page, that opens in the browser, and lets the user play. The rules for Blackjack are here, it’s a relatively simple game. Your implementation just needs to give the "hit" and "stand" options to the player (no betting), feel free to figure out what UI you prefer, as long as the user understands what is happening and can play multiple games.
 
-function setOfCards() {
-
   // 00: How do you store a deck of cards?
   let spread = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
   let colour = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-  let cards = [];
+  var cards  = [];
 
   /* I would like to get the following data structure: (Array with Objects), arrays can make use of the .push() and .pop()-methods which operate rather fast. Arrays store an order in keys, which also can be randomized (or shuffled)...
   cards [
       {colour: Hearts, value: 1}, {colour: Hearts, value: 2}...
     ]
   */
+
+function setOfCards() {
 
   //01: lets make use of an object constructor function: (https://javascript.info/constructor-new)
   function Card(spread,colour){
@@ -34,45 +34,42 @@ function setOfCards() {
   }
 
   //02: lets create a new array cards[] where we store all our cards as indivudual objects
-  //var firstCards = new Card(spread[0], colour[0]);
-  //console.log(firstCards.spread, firstCards.colour);
-
   //03: loop throgh the whole sprad of cards per colour
   for (let j = 0; j < colour.length; j++){
     for (let i = 0; i < spread.length; i++){
       cards.push( new Card(spread[i], colour[j]) );
     }
   }
-
-
-//Use a github function for shuffle, lets exchange it later...
-function fisherYates(array) {
-  var rnd, temp;
-
-  for (var i = array.length - 1; i; i--) {
-    rnd = Math.random() * i | 0;
-    temp = array[i];
-    array[i] = array[rnd];
-    array[rnd] = temp;
+  //Use a github function for shuffle, lets exchange it later...
+  function fisherYates(array) {
+    var rnd, temp;
+    for (var i = array.length - 1; i; i--) {
+      // rnd uses bitwise or: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#.7c_%28Bitwise_OR%29
+      rnd = Math.random() * i | 0;
+      temp = array[i];
+      array[i] = array[rnd];
+      array[rnd] = temp;
+    }
   }
-
-  return array;
+  fisherYates(cards);
+  console.log(cards);
 }
 
 
-  //04: now lets examine functionality below:
-  
-    // simple imaginary first hand deal:
-    console.log(fisherYates(cards));
-    console.log("Dealers hand: \n" + cards[0].name + "\n" + cards[1].name);
-    console.log("Your hand: \n" + cards[2].name + "\n" + cards[3].name +"\n");
+function dealHand(){
+  dealLimit = cards.length;
+  console.log(cards.pop());
+  console.log(dealLimit + "left");
 
-  // calculate risks and chances for first hand of 52 shuffled cards:
-  var riskGeneral = (1 / cards.length) *100;
-  console.log(riskGeneral.toFixed(2) + "% per " + cards.length +" card");
-
-
-
-
-  //return cards;
 }
+
+function onButtonDeal(){
+  let dealButton = document.querySelector("a.deal");
+  let dealButtonClick = dealButton.addEventListener('click', function(){
+    if(dealLimit > 1) {
+			dealHand();
+		} else {
+      console.log('wrogn');
+    }
+  });
+};
